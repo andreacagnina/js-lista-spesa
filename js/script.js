26761050c3e2c543e1145ac6acd97a67592bad29
andreacagnina <amirkal@libero.it>
2024-07-02 17:00:55 +0200

    [FEAT] COMPLETED

--- js/script.js
+++ js/script.js
@@ -9,7 +9,14 @@ while (i < list.length) {
     i++;
     console.log(list[i - 1])
 
-    let li = document.createElement('li').innerHTML.toUpperCase;
+    let li = document.createElement('li');
+
+    li.innerText = list[i - 1];
+
+    console.log(li);
+
+    ul.append(li);
+
 
 
 }
